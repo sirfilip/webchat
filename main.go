5ece9d404fbe1e00350612408d81590f7a906044
Filip Kostovski <github.sirfilip@gmail.com>
2017-10-29 00:49:10 +0200

    Move handlers to functions and add middleware

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"crypto/sha256"
 	"database/sql"
 	"errors"
+	"fmt"
 	"html/template"
 	"io"
 	"net/http"
@@ -14,16 +15,49 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+// const
+const SESSION_NAME string = "default"
+
 // variables
 var store = sessions.NewCookieStore([]byte("my super duper secret"))
-var db *sql.DB
+var conn *sql.DB
 var templates *template.Template
+
+// init
+func db() *sql.DB {
+	var err error
+	if conn == nil {
+		fmt.Println("Connecting to db")
+		conn, err = sql.Open("sqlite3", ":memory:")
+		if err != nil {
+			panic(err)
+		}
+	}
+	return conn
+}
 
 // helpers
 func encrypt(value string) string {
 	h := sha256.New()
 	io.WriteString(h, value)
 	return string(h.Sum(nil))
+}
+
+func current_user(r *http.Request) *User {
+	session, err := store.Get(r, SESSION_NAME)
+	if err != nil {
+		panic(err)
+	}
+	user_id := session.Values["user_id"]
+	user_id64, ok := user_id.(int64)
+	if !ok {
+		panic(errors.New("Failed to convert user id into int64"))
+	}
+	user, err := UserRepo(db()).Find(user_id64)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return user
 }
 
 func createSchema(db *sql.DB) {
@@ -151,6 +185,14 @@ func UserRepo(db *sql.DB) *userRepo {
 	return &userRepo{db}
 }
 
+func (this *userRepo) Find(id int64) (*User, error) {
+	user := &User{}
+	if err := this.db.QueryRow("SELECT id, username, password FROM users WHERE id = ?", id).Scan(&user.ID, &user.Username, &user.Password); err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
 func (this *userRepo) Register(username string, password string) (*User, error) {
 	var encryptedPassword string
 	stmt, err := this.db.Prepare("INSERT INTO users (username, password) VALUES (?, ?)")
@@ -194,104 +236,134 @@ func NewPage(title string) Page {
 
 // middlewares
 
+func LoginRequired(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		session, err := store.Get(r, SESSION_NAME)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		user_id := session.Values["user_id"]
+		if user_id == nil {
+			http.Redirect(w, r, "/login", 301)
+			return
+		}
+		h(w, r)
+	}
+}
+
+// handlers
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	page := NewPage("Home")
+	page.User = current_user(r)
+	chats, err := ChatRepo(db()).FindAll()
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	page.Content["Chats"] = chats
+	templates.ExecuteTemplate(w, "index.html", page)
+}
+
+func ChatNew(w http.ResponseWriter, r *http.Request) {
+	page := NewPage("New Chat")
+	page.User = current_user(r)
+	chat := &Chat{}
+	if r.Method == "POST" {
+		chat.Name = r.FormValue("chat_name")
+		ChatRepo(db()).Save(chat)
+		http.Redirect(w, r, "/", 301)
+		return
+	}
+	page.Content["Chat"] = chat
+	templates.ExecuteTemplate(w, "new.html", page)
+}
+
+func ChatShow(w http.ResponseWriter, r *http.Request) {
+	page := NewPage("Chat Page")
+	page.User = current_user(r)
+	vars := mux.Vars(r)
+	id, err := strconv.ParseUint(vars["id"], 10, 64)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	chat, err := ChatRepo(db()).Find(int64(id))
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	page.Content["Chat"] = chat
+	templates.ExecuteTemplate(w, "show.html", page)
+}
+
+func Register(w http.ResponseWriter, r *http.Request) {
+	page := NewPage("Register")
+	if r.Method == "POST" {
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+		_, err := UserRepo(db()).Register(username, password)
+		if err == nil {
+			http.Redirect(w, r, "/", 301)
+			return
+		}
+		page.Errors = append(page.Errors, err)
+	}
+	templates.ExecuteTemplate(w, "register.html", page)
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	page := NewPage("Login")
+	if r.Method == "POST" {
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+		user, err := UserRepo(db()).FindByUsername(username)
+		if err != nil {
+			page.Errors = append(page.Errors, errors.New("Wrong username and password combination"))
+		} else if !user.HasPassword(password) {
+			page.Errors = append(page.Errors, errors.New("Wrong username and password combination"))
+		} else {
+			session, err := store.Get(r, SESSION_NAME)
+			if err == nil {
+				session.Values["user_id"] = user.ID
+				session.Save(r, w)
+				http.Redirect(w, r, "/", 301)
+				return
+			} else {
+				page.Errors = append(page.Errors, err)
+			}
+		}
+	}
+	templates.ExecuteTemplate(w, "login.html", page)
+}
+
+func Logout(w http.ResponseWriter, r *http.Request) {
+	session, err := store.Get(r, SESSION_NAME)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	session.Values["user_id"] = nil
+	session.Save(r, w)
+	http.Redirect(w, r, "/", 301)
+}
+
 // main
 func main() {
-	db, err := sql.Open("sqlite3", ":memory:")
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
-	createSchema(db)
+	defer db().Close()
+	createSchema(db())
 
 	templates = template.Must(template.ParseGlob("templates/*.html"))
 
 	router := mux.NewRouter()
 
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		page := NewPage("Home")
-		chats, err := ChatRepo(db).FindAll()
-		if err != nil {
-			http.Error(w, err.Error(), 500)
-			return
-		}
-		page.Content["Chats"] = chats
-		templates.ExecuteTemplate(w, "index.html", page)
-	})
-
-	router.HandleFunc("/chats/new", func(w http.ResponseWriter, r *http.Request) {
-		page := NewPage("New Chat")
-		chat := &Chat{}
-		if r.Method == "POST" {
-			chat.Name = r.FormValue("chat_name")
-			ChatRepo(db).Save(chat)
-			http.Redirect(w, r, "/", 301)
-			return
-		}
-		page.Content["Chat"] = chat
-		templates.ExecuteTemplate(w, "new.html", page)
-	})
-
-	router.HandleFunc("/chats/show/{id}", func(w http.ResponseWriter, r *http.Request) {
-		page := NewPage("Chat Page")
-		vars := mux.Vars(r)
-		id, err := strconv.ParseUint(vars["id"], 10, 64)
-		if err != nil {
-			http.Error(w, err.Error(), 500)
-			return
-		}
-		chat, err := ChatRepo(db).Find(int64(id))
-		if err != nil {
-			http.Error(w, err.Error(), 500)
-			return
-		}
-		page.Content["Chat"] = chat
-		templates.ExecuteTemplate(w, "show.html", page)
-	})
-
-	router.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
-		page := NewPage("Register")
-		if r.Method == "POST" {
-			username := r.FormValue("username")
-			password := r.FormValue("password")
-			_, err := UserRepo(db).Register(username, password)
-			if err == nil {
-				http.Redirect(w, r, "/", 301)
-				return
-			}
-			page.Errors = append(page.Errors, err)
-		}
-		templates.ExecuteTemplate(w, "register.html", page)
-	})
-
-	router.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-		page := NewPage("Login")
-		if r.Method == "POST" {
-			username := r.FormValue("username")
-			password := r.FormValue("password")
-			user, err := UserRepo(db).FindByUsername(username)
-			if err != nil {
-				page.Errors = append(page.Errors, errors.New("Wrong username and password combination"))
-			} else if !user.HasPassword(password) {
-				page.Errors = append(page.Errors, errors.New("Wrong username and password combination"))
-			} else {
-				session, err := store.Get(r, "default")
-				if err == nil {
-					session.Values["user_id"] = user.ID
-					session.Save(r, w)
-					http.Redirect(w, r, "/", 301)
-					return
-				} else {
-					page.Errors = append(page.Errors, err)
-				}
-			}
-		}
-		templates.ExecuteTemplate(w, "login.html", page)
-	})
-
-	router.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
-
-	})
+	router.HandleFunc("/", LoginRequired(Home))
+	router.HandleFunc("/chats/new", LoginRequired(ChatNew))
+	router.HandleFunc("/chats/show/{id}", LoginRequired(ChatShow))
+	router.HandleFunc("/register", Register)
+	router.HandleFunc("/login", Login)
+	router.HandleFunc("/logout", LoginRequired(Logout))
 
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("public"))))
 	http.Handle("/", router)
